425c21c646c6da8bcbb56e374461262db7ae78e7
nkyungm <skarudals27@naver.com>
2022-09-21 19:15:32 +0900

    Recursive Call

--- src/Baekjoon_15649.java
+++ src/Baekjoon_15649.java
@@ -1,19 +1,41 @@
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Baekjoon_15649 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int N=sc.nextInt();
         int M=sc.nextInt();
+        permutation(N,M,new ArrayList<>());
 
-        permutation(N,M);
     }
 
-    private static void permutation(int n,int m) {
-        int count=0;
-        for(int i=count;i<n;i++){
-            permutation(n-1,m-1);
+    private static void permutation(int n, int m, ArrayList<Integer> arr) {
+        if(m==0){
+            ArrayList<Integer> arr2=new ArrayList<Integer>();
+            for(int i=0; i<arr.size(); i++) {
+                if(!arr2.contains(arr.get(i)))
+                    arr2.add(arr.get(i));
+            }
+            if(arr2.size()==arr.size())
+            {
+                StringBuilder str = new StringBuilder();
+                for (Integer num : arr2) {
+                    str.append(num);
+                    str.append(" ");
+                }
+                System.out.println(str);
+            }
+
+
+            return;
+        }
+
+        for(int i=1;i<=n;i++){
+
+            arr.add(i);
+            permutation(n,m-1,arr);
+            arr.remove(arr.size()-1); //한번 다 넣은다음 앞의 리스트 삭제하는 작업
         }
     }
-
 }
