5bccf6211ac271a6bed7b5d32d065d73ba98f8e2
nkyungm <skarudals27@naver.com>
2022-09-23 00:03:40 +0900

    BruteForce Algorithm

--- src/Baekjoon_1018_1.java
+++ src/Baekjoon_1018_1.java
@@ -1,10 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Baekjoon_1018 {
+public class Baekjoon_1018_1 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
---
+++ src/Baekjoon_1018_2.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Baekjoon_1018_2 {
+    static char chess[][];
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        chess = new char[N][M];
+        sc.nextLine();
+
+        for (int i = 0; i < N; i++) {
+            String s = sc.nextLine();
+            for (int j = 0; j < M; j++) {
+                chess[i][j] = s.charAt(j);
+            }
+        }
+
+        int ans = 1000000;
+
+        for (int i = 0; i < N-7; i++) {
+            for (int j = 0; j < M-7; j++) {
+                ans = Math.min(ans, f(i,j));
+            }
+        }
+        System.out.println(ans);
+    }
+    public static int f(int x, int y) {
+        int B = 0;
+        int W = 0;
+
+        for (int i = x; i < x+8; i++) {
+            for (int j = y; j < y+8; j++) {
+                if((i + j) % 2 == 0) {
+                    if(chess[i][j] == 'B') W++;
+                    else B++;
+                }else { // (i + j) % 2 == 1)
+                    if(chess[i][j] == 'W') W++;
+                    else B++;
+                }
+            }
+        }
+        return Math.min(B,W);
+    }
+
+}
