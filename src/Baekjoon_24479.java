ea11152de5c0d431bb68394227a5e3777ac26b6f
nkyungm <skarudals27@naver.com>
2022-09-26 21:26:44 +0900

    DFS(Depth First Search) Algorithm

--- src/Baekjoon_24479.java
+++ src/Baekjoon_24479.java
@@ -1,32 +1,56 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class Baekjoon_24479 {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int N=sc.nextInt();
-        int M=sc.nextInt();
-        int R=sc.nextInt();
-        boolean [] visited=new boolean[N+1];
-        //int[][] E= new int[N][M];
-        int[][] E= {{},{2,5},{3,5},{5},{},{}};
-//        for(int i=0;i<M;i++){
-//
-//        }
-        dfs(visited,E,1);
-        for(int i=1;i<=N;i++){
-            if(visited[i]==false){
-                System.out.println(0);
-            }
+    int cnt=0; //전역변수
+
+    public void solution() throws Exception { //throws Exception 예외처리(매번 해주는게 좋음)
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        int [] visited=new int[N];
+        ArrayList<Integer>[] E=new ArrayList[N];
+        for(int i=0;i<N;i++){
+            E[i]=new ArrayList<Integer>();
+            visited[i]=0;
         }
+        for(int i=0;i<M;i++){
+            st = new StringTokenizer(br.readLine()); //다시 해야함
+
+            int u=Integer.parseInt(st.nextToken());
+            int v=Integer.parseInt(st.nextToken());
+            E[u-1].add(v-1);
+            E[v-1].add(u-1);
+        }
+        for(int i=0;i<N;i++) {
+            Collections.sort(E[i]);
+        }
+
+        dfs(visited,E,R-1);
+        for(int i=0;i<N;i++){
+            System.out.println(visited[i]);
+        }
+
     }
-    public static void dfs(boolean[] V,int[][] E,int R) {
-        V[R]=true;
-        System.out.printf("%d\n",R);
+    void dfs(int[] V,ArrayList<Integer>[] E,int R) {
+
+        V[R]=++cnt;
+        //System.out.printf("%d\n",R+1);
         for(int node:E[R]){
-             if(V[node]==false){
+            if(V[node]==0){
                 dfs(V,E,node);
             }
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
     }
 }
